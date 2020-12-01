7fd0aa87bfe44f370209fae9340476dd01522cac
Piet Cordemans <cordemans.piet@gmail.com>
2020-12-01 11:58:48 +0100

    timer 7 code example with interrupts

---
+++ README.md
@@ -0,0 +1,25 @@
+# Low Level Blinky
+
+Low level blinky example for the NUCLEO L476RG
+
+## Installation
+
+[GNU ARM Embedded toolchain](https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-rm)
+
+```
+sudo apt install gcc-arm-none-eabi
+```
+
+## Building the example
+
+```
+make
+```
+
+## Flash to the NUCLEO L476RG
+
+Copy the **.bin** file in the BUILD folder to the NUCLEO drive
+
+## More information
+
+https://microcontrollers.netlify.app/digital-io
---
+++ main.cpp
@@ -0,0 +1,56 @@
+// includes low level peripheral definitions
+#include "stm32l476xx.h"
+
+//All IRQ handlers are weakly defined by the compiler as C functions
+//As C has different function calling conventions then C++, the code block has to be marked as extern C
+extern "C" {
+    // Interrupt handler is called when the interrupt has been raised
+    void TIM7_IRQHandler(){
+        // clear TIM 7 interrupt flag
+        TIM7->SR = 0;
+        
+        if(GPIOA->ODR & (1<<5)){
+            //GPIOA_BSRR reset pin 5: set bit 21
+            GPIOA->BSRR |= 1 << 21;
+        }
+        else {
+            //GPIOA_BSRR set pin 5: set bit 5
+            GPIOA->BSRR |= 1 << 5;
+        }
+    }
+}
+
+int main()
+{    
+    //Green led of the NUCLEO-L476RG is connected to PA5
+    //Enable GPIOA peripheral in the AHB2ENR: set bit 0
+    RCC->AHB2ENR |= 1;
+
+    //Enable TIM7 in the APB1ENR1: set bit 5
+    RCC->APB1ENR1 |= 1 << 5;
+    
+    // GPIOA_MODER set GP output mode: reset bit 11 & set bit 10
+    GPIOA->MODER &= ~(1<<11);
+    GPIOA->MODER |= 1 << 10;
+        
+    // Enable the global interrupt for TIM7 in NVIC
+    NVIC_EnableIRQ(TIM7_IRQn);
+
+    // Given a clock of 48MHz, set prescaler and count register for a period of 1s
+    TIM7->PSC = 1999;
+    TIM7->ARR = 23999;
+    
+    // Set event generation, re-initializes timer counter and generates an update of the registers
+    TIM7->EGR |= 1;
+    
+    // Enable interrupt in TIM7
+    TIM7->DIER |= 1;
+    
+    // Start TIM7
+    TIM7->CR1 |= 1;
+
+    while (true)
+    {
+        //wait here for interrupts
+    }    
+}
